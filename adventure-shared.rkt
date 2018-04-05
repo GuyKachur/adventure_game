b6a14080f2d998d910522dcfb216df933795a081
Guy Kachur <gkachur@gmail.com>
2018-04-05 15:13:04 -0700

    Added shared file
    added shared file for impending large reformatting of the game

---
+++ adventure-shared.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+
+
+;                                                  
+;                                                  
+;                                                  
+;                                                  
+;    ;;; ;  ;;                                  ;; 
+;   ;   ;;   ;                                   ; 
+;   ;        ; ;;    ;;;;   ;; ;;;   ;;;;    ;;; ; 
+;    ;;;;    ;;  ;  ;    ;   ;;     ;    ;  ;   ;; 
+;        ;   ;   ;   ;;;;;   ;      ;;;;;;  ;    ; 
+;        ;   ;   ;  ;    ;   ;      ;       ;    ; 
+;   ;;   ;   ;   ;  ;   ;;   ;      ;       ;   ;; 
+;   ; ;;;   ;;; ;;;  ;;; ;; ;;;;;    ;;;;;   ;;; ;;
+;                                                  
+;                                                  
+;                                                  
+;                                                  
+
+#| This is all the functions that will be shared between the server and the client for the game weve built |#
+
+
